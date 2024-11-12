03b657f4cc20c20101ab89640b956cef3fb7307b
mahdifire1403 <dft.ahadi@gmail.com>
2024-11-12 07:17:28 +0330

    i set variables and if statements

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if random_Number == 1:
 if random_Number == 2:
     computer_Move = "scissors"
     print("compute_Move")
-    player_1 = input(f"player_1: make your move,{computer_Move}")
-    player_2 = input("player_1:make your move")
+    player_1 = input(f"player_1 : make your move , {computer_Move}")
+    player_2 = input(f"player_1 : make your move , {computer_Move}")

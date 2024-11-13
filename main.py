1a28314ec2c689c774e7eeb2e3fd7cbf448850f3
mahdifire1403 <dft.ahadi@gmail.com>
2024-11-13 21:36:09 +0330

    i set moves of computer and player and set rules of this game

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
-
+print("Rock".lower())
+print("Paper".lower())
+print("Scissors".lower())
+print("__________________________")
 random_Number = random.randint(0, 2)
 if random_Number == 0:
     computer_Move = "rock"
@@ -7,9 +10,11 @@ if random_Number == 1:
     computer_Move = "paper"
 if random_Number == 2:
     computer_Move = "scissors"
-    print("compute_Move")
-    player_1 = input(f"player_1, make your move: {computer_Move}")
-    player_2 = input(f"player_2, make your move: {computer_Move}")
+    # print("compute_Move")
+    player_1 = input("player_1, make your move:")
+    player_2 = computer_Move
+    print(f"player_2, make your move: {computer_Move}")
+
     if player_1 == player_2:
         print("that is a tie")
     elif player_1 == "rock":

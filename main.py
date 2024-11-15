a77d7206a6cd70c24e4fc2c73e4c51802c8ccfa3
mahdifire1403 <dft.ahadi@gmail.com>
2024-11-15 15:12:09 +0330

    i set suitable comments for the program

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ print("Rock".lower())
 print("Paper".lower())
 print("Scissors".lower())
 print("__________________________")
+# set random
 random_Number = random.randint(0, 2)
+# set moves for computer
 if random_Number == 0:
     computer_Move = "rock"
 if random_Number == 1:
@@ -11,10 +13,12 @@ if random_Number == 1:
 if random_Number == 2:
     computer_Move = "scissors"
     # print("compute_Move")
+    # set player1
     player_1 = input("player_1, make your move:")
+    # set player2
     player_2 = computer_Move
     print(f"player_2, make your move: {computer_Move}")
-
+# set rules of players
     if player_1 == player_2:
         print("that is a tie")
     elif player_1 == "rock":
